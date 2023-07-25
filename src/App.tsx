68e06de01b5d2d09c3ecf37a56bb42776489988f
Elioenay <elionay407@gmail.com>
2023-07-25 10:35:33 -0300

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
+export default function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Coffee Delivery</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
